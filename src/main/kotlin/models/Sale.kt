0d5f02cd02a63fa59b474e1c5e7c4a0d2ba2d84e
VopRos-ZP <zolpavel366@gmail.com>
2023-09-28 08:47:12 +0300

    update Sale

--- src/main/kotlin/models/Sale.kt
+++ src/main/kotlin/models/Sale.kt
@@ -1,16 +1,11 @@
 package models
 
-import kotlinsql.annotations.Column
-import kotlinsql.annotations.PrimaryKey
-import java.sql.Date
-import java.time.LocalDate
+import kotlinx.datetime.LocalDate
+
 
 data class Sale(
-    @PrimaryKey var id: Int = 1,
-    @Column("registration_number") var registrationNumber: Int = 0,
-    @Column var date: Date = Date.valueOf(LocalDate.now()),
-    @Column("product_id") var productId: Int = 1,
-    @Column var firstname: String = "",
-    @Column var lastname: String = "",
-    @Column var patronymic: String = ""
+    val id: Int,
+    val date: LocalDate,
+    val product: Product,
+    val fio: String
 )
